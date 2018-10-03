6dda7409a083fd561cfd4117223cc0ad03266f98
goreleaserbot <goreleaser@carlosbecker.com>
2018-10-03 09:10:42 +0100

    Brew formula update for rae version v0.0.1

--- Formula/rae.rb
+++ Formula/rae.rb
@@ -3,13 +3,14 @@ class Rae < Formula
   homepage "https://github.com/LGUG2Z/rae"
   url "https://github.com/LGUG2Z/rae/releases/download/v0.0.1/rae_0.0.1_darwin_amd64.tar.gz"
   version "0.0.1"
-  sha256 "1bfea236834b3e2da6316ade09989f46243c49fd6f3dc4606d7305b9d2ac86df"
+  sha256 "a3a95a42c257a4802b13fdbf2672d00547842dc5f4c52e1616e003598f03f697"
   
   depends_on "docker"
 
   def install
     bin.install "rae"
-    bash_completion.install "bash_autocopmlete"
+    bash_completion.install "bash_autocomplete"
+    zsh_completion.install "zsh_autocomplete"
   end
 
   test do
