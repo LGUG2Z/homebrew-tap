4f0b9b217bd0a79179d0e991ae88f9b09ce345e1
goreleaserbot <goreleaser@carlosbecker.com>
2018-10-06 18:40:02 +0100

    Brew formula update for rae version v0.0.3

--- Formula/rae.rb
+++ Formula/rae.rb
@@ -1,9 +1,9 @@
 class Rae < Formula
   desc "A docker-compose development environment orchestrator"
   homepage "https://github.com/LGUG2Z/rae"
-  url "https://github.com/LGUG2Z/rae/releases/download/v0.0.2/rae_0.0.2_darwin_amd64.tar.gz"
-  version "0.0.2"
-  sha256 "73b7c6c9d01be2ec4e955131224c6d56250d4327edf504f5787a1be9fcd1a8d9"
+  url "https://github.com/LGUG2Z/rae/releases/download/v0.0.3/rae_0.0.3_darwin_amd64.tar.gz"
+  version "0.0.3"
+  sha256 "1b86b8a5945adbb505b5d7db8fece432ad946291252925143b9810dc8689546c"
   
   depends_on "docker"
 
