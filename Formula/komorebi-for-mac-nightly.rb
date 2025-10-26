1b5c7f43316cdd3ee7b4375a30a5f506ea5037d2
LGUG2Z <LGUG2Z@fastmail.com>
2025-10-26 13:38:02 -0700

    spdx is a joke

--- Formula/komorebi-for-mac-nightly.rb
+++ Formula/komorebi-for-mac-nightly.rb
@@ -1,17 +1,15 @@
 class KomorebiForMacNightly < Formula
-  desc "A tiling window manager for macOS (nightly build)"
+  desc "Tiling window manager for macOS (nightly build)"
   homepage "https://github.com/KomoCorp/komorebi-for-mac"
-  version "nightly"
-  license "Komorebi License 2.0.0"
-
   url "https://api.github.com/repos/KomoCorp/komorebi-for-mac/releases/assets/308949887",
       headers: [
         "Accept: application/octet-stream",
         "X-GitHub-Api-Version: 2022-11-28",
-        "Authorization: bearer #{ENV.fetch('HOMEBREW_GITHUB_API_TOKEN')}"
+        "Authorization: bearer #{ENV.fetch("HOMEBREW_GITHUB_API_TOKEN")}",
       ]
-
+  version "nightly"
   sha256 "d91583e5479ed0a23bfb9c7253e8f1f32dcedc0e0d1b886ff54d43894f670724"
+  license "Komorebi License 2.0.0"
 
   def install
     bin.install Dir["*"]
