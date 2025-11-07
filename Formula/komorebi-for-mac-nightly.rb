5b1340f58387515d0e80f9bc86c7c793258f71a4
JeezyBot <lgug2z@fastmail.com>
2025-11-06 18:35:57 -0800

    Update komorebi-for-mac-nightly to 313564708 (ac4fee0243efd7de9392c1946bc37158d9d20bd6cc56753f1ab798ca45cfd212)

--- Formula/komorebi-for-mac-nightly.rb
+++ Formula/komorebi-for-mac-nightly.rb
@@ -1,18 +1,22 @@
 class KomorebiForMacNightly < Formula
   desc "Tiling window manager for macOS (nightly build)"
   homepage "https://github.com/KomoCorp/komorebi-for-mac"
-  url "https://api.github.com/repos/KomoCorp/komorebi-for-mac/releases/assets/312920276",
+  url "https://api.github.com/repos/KomoCorp/komorebi-for-mac/releases/assets/313564708",
       headers: [
         "Accept: application/octet-stream",
         "X-GitHub-Api-Version: 2022-11-28",
         "Authorization: bearer #{ENV.fetch("HOMEBREW_GITHUB_API_TOKEN")}",
       ]
-  version "0.1.0-nightly-2025-11-05-08:38:46"
-  sha256 "559766bab68079ead7ef080479615c5339dcc2c5af2e63243a21599e999053ad"
+  version "0.1.0-nightly-2025-11-06-18:35:55"
+  sha256 "ac4fee0243efd7de9392c1946bc37158d9d20bd6cc56753f1ab798ca45cfd212"
   license "Komorebi License 2.0.0"
 
   def install
     bin.install Dir["*"]
+
+    system "codesign", "--force", "-s", "-", "#{bin}/komorebi"
+    system "codesign", "--force", "-s", "-", "#{bin}/komorebic"
+    system "codesign", "--force", "-s", "-", "#{bin}/komorebi-bar"
   end
 
   def caveats
