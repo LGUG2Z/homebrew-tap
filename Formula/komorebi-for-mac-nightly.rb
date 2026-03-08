332fdb59d86869bc24dee0e725802b4283fff21d
JeezyBot <lgug2z@fastmail.com>
2026-03-07 16:56:42 -0800

    Update komorebi-for-mac-nightly to 369180352 (1df89487023e3e9fac729166d87d860724ffff630ef7586f0578e4b31441ab05)

--- Formula/komorebi-for-mac-nightly.rb
+++ Formula/komorebi-for-mac-nightly.rb
@@ -1,14 +1,14 @@
 class KomorebiForMacNightly < Formula
   desc "Tiling window manager for macOS (nightly build)"
   homepage "https://github.com/KomoCorp/komorebi-for-mac"
-  url "https://api.github.com/repos/KomoCorp/komorebi-for-mac/releases/assets/348938723",
+  url "https://api.github.com/repos/KomoCorp/komorebi-for-mac/releases/assets/369180352",
       headers: [
         "Accept: application/octet-stream",
         "X-GitHub-Api-Version: 2022-11-28",
         "Authorization: bearer #{ENV.fetch("HOMEBREW_GITHUB_API_TOKEN")}",
       ]
-  version "0.1.0-nightly-2026-01-31-19:39:32"
-  sha256 "acba8c190dca45497f82e43a5b5eee529dc90fd5b1895351a6c4d40b30eba05f"
+  version "0.1.0-nightly-2026-03-07-16:56:40"
+  sha256 "1df89487023e3e9fac729166d87d860724ffff630ef7586f0578e4b31441ab05"
   license "Komorebi License 2.0.0"
 
   def install
