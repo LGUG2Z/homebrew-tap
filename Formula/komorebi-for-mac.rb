c892e7e8e19f6f6c61d47b7a53423ae45f972b86
LGUG2Z <LGUG2Z@fastmail.com>
2026-05-10 12:01:55 -0700

    Update komorebi-for-mac to v0.1.1

--- Formula/komorebi-for-mac.rb
+++ Formula/komorebi-for-mac.rb
@@ -1,8 +1,8 @@
 class KomorebiForMac < Formula
   desc "Tiling window manager for macOS"
   homepage "https://github.com/LGUG2Z/komorebi-for-mac"
-  url "https://github.com/LGUG2Z/komorebi-for-mac/releases/download/v0.1.0/komorebi-v0.1.0-aarch64-apple-darwin.zip"
-  sha256 "4afa1575c35b084e3147013f081ac3f2704c3abddf8a7ce4a65dbd24982c7073"
+  url "https://github.com/LGUG2Z/komorebi-for-mac/releases/download/v0.1.1/komorebi-v0.1.1-aarch64-apple-darwin.zip"
+  sha256 "b11209203b5baf3c8f3b1099062e61e73e5afacf5d1c104e7e3b73dcc8296b71"
   license "Komorebi License 2.0.0"
 
   def install
